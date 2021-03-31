a6ea50bd7429e0f34e34a7ae6dc6bad395db55b6
tusharsingla002 <76396067+tusharsingla002@users.noreply.github.com>
2021-03-31 23:53:03 +0530

    Delete Folder directory

--- Folder/java.js
+++
@@ -1,8 +0,0 @@
-Current Date:
-<input type="text" id="currentDate">
-<br><br>
-<script>
-  var today = new Date();
-  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-  document.getElementById("currentDate").value = date;
-</script>
